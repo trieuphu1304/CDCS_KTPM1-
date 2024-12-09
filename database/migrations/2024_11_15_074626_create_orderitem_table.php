78b0f0b3c418fd65039c3464809b864410217425
TrieuPhu <trieuphu130404@gmail.com>
2024-12-09 14:41:56 +0700

    Update migrations: removed detail orders table, added order item table

--- database/migrations/2024_11_15_074626_create_orderitem_table.php
+++ database/migrations/2024_11_15_074626_create_orderitem_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('detail_oders', function (Blueprint $table) {
+        Schema::create('orderitem', function (Blueprint $table) {
             $table->id();
             $table->decimal('price',total: 15, places: 2);
             $table->integer('quantity');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('detail_oders');
+        Schema::dropIfExists('orderitem');
     }
 };
